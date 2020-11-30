505c9dcdfca05246e53a8bccc11f9806b700c15b
Maxim Bazant <maxim.bazant@outlook.com>
2020-11-30 14:55:01 +0100

    starting player movement and game class

--- Game.py
+++ Game.py
@@ -1,31 +1,51 @@
+from Player import Player
 import pygame
-from Player import *
 
-# VARIABLES
-win_width = 1600
-win_height = 1100
 
-win = pygame.display.set_mode((win_width, win_height))
+class Game:
+    def __init__(self):
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.win_width = 1600
+        self.win_height = 1100
+        self.win = pygame.display.set_mode((self.win_width, self.win_height))
+        self.FPS = 60
+        self.player = Player()
 
-clock = pygame.time.Clock()
-FPS = 60
+    def start_screen(self):
+        pass
 
-running = True
+    def game_over_screen(self):
+        pass
+
+    def game(self):
+        self.handle_events()
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.player.move_left()
+        if keys[pygame.K_RIGHT]:
+            self.player.move_right()
+        if keys[pygame.K_RCTRL]:
+            self.player.jump()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def run(self):
+        self.start_screen()
+        self.game()
+        self.game_over_screen()
+
+
+g = Game()
 
 # MAIN LOOP
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        print("left")
-    if keys[pygame.K_RIGHT]:
-        print("right")
-    if keys[pygame.K_RCTRL]:
-        print("jumping")
+while g.running:
+    g.run()
 
 
 
--- settings.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-win_width = 1600
-win_height = 1100
-
-win = pygame.display.set_mode((win_width, win_height))
-
-clock = pygame.time.Clock()
-FPS = 60
