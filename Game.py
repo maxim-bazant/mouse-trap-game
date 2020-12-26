2bd19dcee458e573cb5dac55f6a70c79ec67538f
Maxim Bazant <maxim.bazant@outlook.com>
2020-12-26 16:11:31 +0100

    pistons done

--- Game.py
+++ Game.py
@@ -55,6 +55,11 @@ class Game:
 
                               FallingFloor(0, self.floor[2].y + 5)]
 
+        self.pistons = [Piston(self.floor[1].x - self.floor[1].width, self.floor[1].y + self.floor[1].height),
+                        Piston(self.floor[1].x - self.floor[1].width + 125, self.floor[1].y + self.floor[1].height),
+                        Piston(self.floor[1].x - self.floor[1].width + 250, self.floor[1].y + self.floor[1].height),
+                        Piston(self.floor[1].x - self.floor[1].width + 375, self.floor[1].y + self.floor[1].height)]
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -161,6 +166,9 @@ class Game:
 
         for ball in self.balls:
             ball.show_and_move()
+
+        for piston in self.pistons:
+            piston.show_and_move()
 
         self.flower.show_me()
 
--- Piston.py
+++ Piston.py
@@ -1,4 +1,52 @@
+from settings import *
+
+
+class Tail(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("images/piston_tail.png")
+        self.width = self.image.get_rect().width
+        self.height = self.image.get_rect().height
+
+    def show_tail(self):
+        win.blit(self.image, (self.x, self.y))
 
 
 class Piston(object):
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("images/piston.png")
+        self.width = self.image.get_rect().width
+        self.height = self.image.get_rect().height
+        self.mc_default = 250
+        self.move_count = self.mc_default
+        self.vel = 1.5
+        self.tail = [Tail(self.x + self.width // 2 - 12, self.y)]
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def show_and_move(self):
+        neg = 1
+        if self.move_count >= self.mc_default // 2:
+            neg = -1
+        elif self.move_count <= self.mc_default // 2:
+            neg = 1
+        if self.move_count == 0:
+            self.move_count = self.mc_default
+
+        self.move_and_show_tail(neg)
+
+        self.move_count -= 1
+        self.y -= self.vel * neg
+
+        win.blit(self.image, (self.x, self.y))
+
+    def move_and_show_tail(self, neg):
+        for tail in self.tail:
+            tail.show_tail()
+
+        if self.y - (self.tail[-1].y + self.tail[-1].height) > -5 and neg == -1:
+            self.tail.append(Tail(self.tail[-1].x, self.tail[-1].y + self.tail[-1].height))
+        elif self.y - self.tail[-1].y < -5 and neg == 1:
+            self.tail.pop(-1)
