d67307a2d6d2923e0236aca6b5df70ea4808fbf3
Maxim Bazant <maxim.bazant@outlook.com>
2021-01-09 18:19:07 +0100

    small changes

--- Player.py
+++ Player.py
@@ -133,6 +133,8 @@ class Player:
         win.blit(self.dying_images[self.dying_count // self.image_changer], (self.x, self.y))
 
         if self.dying_count // self.image_changer == 17:
+            self.move_left_bc_tha_wall = False
+            self.move_right_bc_tha_wall = False
             self.facing_right = False
             self.facing_left = True
             self.dying = False
