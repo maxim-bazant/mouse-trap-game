b1fabc3f5b22507ebebe3092ee4165f4b0ab05ac
Maxim Bazant <maxim.bazant@outlook.com>
2021-01-15 14:27:13 +0100

    main music

--- settings.py
+++ settings.py
@@ -7,3 +7,11 @@ FPS = 100
 win = pygame.display.set_mode((win_width + side_width, win_height))
 
 bg_color = (0, 120, 130)  # color most similar to original bg color
+
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.font.init()
+pygame.init()
+
+bg_music = pygame.mixer.music.load("sound/music.mp3")
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1)  # -1 means that it will play the music infinitely
